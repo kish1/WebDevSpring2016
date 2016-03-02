fa7c5a1dc64fcdb728806fcf59f4da5c464a0a11
Kishore Sridhar <kish.connect@gmail.com>
2016-03-02 18:17:59 -0500

    hw2: sidebar and admin done

--- public/assignment/views/admin/admin.controller.js
+++ public/assignment/views/admin/admin.controller.js
@@ -1,3 +1,13 @@
 /**
  * Created by kishore on 3/1/16.
  */
+"use strict";
+(function () {
+    angular
+        .module("FormBuilderApp")
+        .controller("AdminController", AdminController);
+
+    function AdminController() {
+
+    }
+})();
--- public/assignment/views/sidebar/sidebar.controller.js
+++ public/assignment/views/sidebar/sidebar.controller.js
@@ -7,9 +7,21 @@
         .module("FormBuilderApp")
         .controller("SidebarController", SidebarController);
 
-    function SidebarController($scope, $location) {
+    function SidebarController($scope, $location, $rootScope) {
         $scope.$location = $location;
+        $scope.isLoggedIn = isLoggedIn;
+        $scope.isAdmin = isAdmin;
 
+        function isLoggedIn() {
+            return $rootScope.currentUser != null;
+        }
+
+        function isAdmin() {
+            if ($rootScope.currentUser) {
+                return ($rootScope.currentUser.roles.indexOf("admin") > -1);
+            }
+            return false;
+        }
 
     }
 })();
