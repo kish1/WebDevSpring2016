7f045f027ff898ccb706c1052ca5c19275c9097c
Kishore Sridhar <kish.connect@gmail.com>
2016-03-06 00:08:02 -0500

    apitest: need to use angular

--- public/experiments/youtubeapi/app.js
+++ public/experiments/youtubeapi/app.js
@@ -3,6 +3,10 @@
  */
 "use strict";
 
+$(function () {
+
+});
+
 function clientReady(){
     gapi.client.setApiKey("AIzaSyCx9cGg3yPKiWiMJyUGRAxKCCg5hlMKOqw");
     gapi.client.load("youtube", "v3", function () {});
