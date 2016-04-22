a6f59a7b6698c7cf2d1abd56d675f90b18bffcd6
Kishore Sridhar <kish.connect@gmail.com>
2016-04-22 15:39:23 -0400

    project: modified imageUrl in profile controller

--- public/project/client/views/users/profile.controller.js
+++ public/project/client/views/users/profile.controller.js
@@ -70,7 +70,7 @@
         }
 
         function imageUrl(imageName) {
-            return "http://" +  location.host +  "/project/server/images/dp/" + imageName;
+            return "http://" +  location.host +  "/images/dp/" + imageName;
         }
 
     }
